8702cc14262f31d24525fb17694cb6dd493845d8
kotze.kobus13@gmail.com <KobusKotze13>
2023-09-23 15:32:39 +0200

    Finish off game

--- pong.py
+++ pong.py
@@ -15,12 +15,13 @@ WHITE = (255,255,255)
 BLACK = (0,0,0)
 BALL_RADIUS = 8
 SCORE_FONT = pygame.font.SysFont(name="Arial", size=50)
-Player1_Score = 0
-Player2_Score = 0
+WINNING_SCORE = 10
+
 
 class Paddel:
     COLOR = WHITE
-    VEL = 5
+    VEL = 10
+
     def __init__(self, x, y, width,height):
         self.x = x
         self.y = y
@@ -41,6 +42,7 @@ class Ball:
     COLOR = WHITE
     MAX_VEL = 7
 
+
     def __init__(self, x, y, radius):
         self.x = x
         self.y = y
@@ -55,13 +57,23 @@ class Ball:
         self.x += self.x_vel
         self.y += self.y_vel
 
+    def reset(self):
+        if (self.x < 0) or (self.x > WIDTH):
+            self.x = WIDTH//2
+            self.y = HEIGHT//2
+            self.x_vel *= -1
+            self.y_vel = random.randint(-7,7)
         
 
-def draw(win, paddles,ball, p1_score, p2_score):
+
+
+        
+
+def draw(win, paddles, ball, Left_Score, Right_Score ):
     win.fill(BLACK)
 
-    Player1_score_text = SCORE_FONT.render(f'{p1_score}',1,WHITE)
-    Player2_score_text = SCORE_FONT.render(f'{p2_score}',1,WHITE)
+    Player1_score_text = SCORE_FONT.render(f"{Left_Score}",1,WHITE)
+    Player2_score_text = SCORE_FONT.render(f"{Right_Score}",1,WHITE)
     win.blit(Player1_score_text, (WIDTH//4 - Player1_score_text.get_width()//2, 20))
     win.blit(Player2_score_text, (WIDTH - WIDTH//4 - Player2_score_text.get_width()//2, 20))
     
@@ -76,7 +88,7 @@ def draw(win, paddles,ball, p1_score, p2_score):
 
     ball.draw_b(win)
 
-    pygame.display.update()
+    pygame.display.flip()
 
 
 def handle_collision(ball,left_paddle, right_paddle):
@@ -89,27 +101,13 @@ def handle_collision(ball,left_paddle, right_paddle):
         if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
             if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
                 ball.x_vel *= -1
-                ball.y_vel = ball.MAX_VEL - random.randint(0,7)
+                ball.y_vel += random.randint(-3,3)
     
     else:
         if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
             if ball.x + ball.radius >= right_paddle.x:
                 ball.x_vel *= -1
-                ball.y_vel = ball.MAX_VEL - random.randint(0,7)
-
-def ball_reset(ball):
-    if (ball.x  - ball.radius <= 0) or (ball.x + ball.radius >= WIDTH):
-        ball.x = WIDTH//2
-        ball.y = HEIGHT//2
-        ball.x_vel *= -1
-        ball.y_vel = ball.MAX_VEL - random.randint(0,7)
-        if ball.x  - ball.radius <= 0:
-            Player2_Score += 1
-        elif ball.x  + ball.radius >= WIDTH:
-            Player1_Score += 1
-
-
-
+                ball.y_vel += random.randint(-3,3)
 
 
 def handle_paddle_movement(keys, left_paddle, right_paddle):
@@ -128,31 +126,42 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    left_paddle = Paddel(15,(HEIGHT//2) - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
-    right_paddle = Paddel(WIDTH - 15 - PADDLE_WIDTH,(HEIGHT//2) - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    left_paddle = Paddel(10,(HEIGHT//2) - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
+    right_paddle = Paddel(WIDTH - 10 - PADDLE_WIDTH,(HEIGHT//2) - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
     
+    Left_Score = 0
+    Right_Score = 0
 
     
     ball = Ball(WIDTH//2,HEIGHT//2,BALL_RADIUS)
 
     while run:
         clock.tick(FPS)
-        draw(WIN, [left_paddle, right_paddle], ball, Player1_Score, Player2_Score) 
+
+        if ball.x  < 0:
+            Right_Score += 1
+            ball.reset()
+        elif ball.x  > WIDTH:
+            Left_Score += 1 
+            ball.reset()
+
+        if Left_Score >= WINNING_SCORE:
+            pass
+        elif Right_Score >= WINNING_SCORE:
+            pass
+
+        draw(WIN, [left_paddle, right_paddle], ball, Left_Score, Right_Score) 
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                break
-
-                   
+                break   
 
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle, right_paddle)
 
         ball.move()
         handle_collision(ball,left_paddle,right_paddle)
-        ball_reset(ball)
-
 
     pygame.quit()
 
